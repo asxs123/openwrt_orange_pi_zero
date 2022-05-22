3afd3fe585b97a51ac8fe2e561e348b09fa32a46
asxs123 <54346276+asxs123@users.noreply.github.com>
2022-05-22 10:41:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,12 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.31.123/g' package/base-files/files/bin/config_generate
-
-cat >> .config <<EOF
-CONFIG_TARGET_sunxi=y
-CONFIG_TARGET_sunxi_cortexa7=y
-CONFIG_TARGET_sunxi_cortexa7_DEVICE_xunlong_orangepi-zero=y
-EOF
-          # ========================固件定制部分结束========================
-          # 
-sed -i 's/^[ \t]*//g' ./.config
