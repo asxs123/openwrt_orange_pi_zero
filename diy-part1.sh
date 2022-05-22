95532b21111224736f400ff6cae5176753f914c8
asxs123 <54346276+asxs123@users.noreply.github.com>
2022-05-22 09:10:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # add package
-git clone  -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean
+git clone  -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon18.06
